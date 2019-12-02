50261ae0ac206e4f242c7ebb57b55dbc8fe27e4e
Saliy Alexander <isaliyasdg@gmail.com>
2019-12-02 21:47:53 +0300

    Added ProductType

---
+++ AbstractTypes/ProductType.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountAggregator.AbstractTypes
+{
+    public class ProductType
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string VarietyID { get; set; }
+    }
+}
---
+++ Controllers/ProductTypeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductType = DiscountAggregator.AbstractTypes.ProductType;
+
+namespace DiscountAggregator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductTypeController : ControllerBase
+    {
+        private readonly ProductTypeDB _ProductTypeDB;
+
+        public ProductTypeController(ProductTypeDB ProductTypeDB)
+        {
+            _ProductTypeDB = ProductTypeDB;
+        }
+
+
+
+        // GET: api/ProductType
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductType>> Get() =>
+            _ProductTypeDB.Get().ToList();
+
+
+        // GET: api/ProductType/5
+        [HttpGet("{id}", Name = "GetProductType")]
+        public ActionResult<ProductType> Get(string id)
+        {
+            var productType = _ProductTypeDB.Get(id);
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return productType;
+        }
+        [HttpGet("GetFromName/{name}")]
+        public ActionResult<IEnumerable<ProductType>> GetFromName(string name)
+        {
+            var productType = _ProductTypeDB.GetFromName(name).ToList();
+
+            if (productType == null || productType.Count == 0)
+            {
+                return NotFound();
+            }
+            return productType;
+        }
+
+        // POST: api/ProductType
+        [HttpPost]
+        public ActionResult<ProductType> Create(ProductType ProductType)
+        {
+            _ProductTypeDB.Add(ProductType);
+            return CreatedAtRoute("GetProductType", new { id = ProductType.Id.ToString() }, ProductType);
+        }
+
+        // PUT: api/ProductType/5
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, ProductType ProductTypeIn)
+        {
+            var ProductType = _ProductTypeDB.Get(id);
+
+            if (ProductType == null)
+            {
+                return NotFound();
+            }
+            _ProductTypeDB.Update(id, ProductTypeIn);
+
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var ProductType = _ProductTypeDB.Get(id);
+
+            if (ProductType == null)
+            {
+                return NotFound();
+            }
+
+            _ProductTypeDB.Remove(ProductType.Id);
+
+            return NoContent();
+        }
+    }
+}
