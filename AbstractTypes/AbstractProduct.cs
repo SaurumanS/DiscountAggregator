d346b698a31e82addd93a8f89a108bb656132d5c
Saliy Alexander <isaliyasdg@gmail.com>
2019-12-11 22:05:14 +0300

    Selected is working now

--- AbstractTypes/AbstractProduct.cs
+++ AbstractTypes/AbstractProduct.cs
@@ -7,20 +7,14 @@ namespace DiscountAggregator.AbstractTypes
 {
     public abstract class AbstractProduct
     {
-        public string DiscountCounter(string oldPrice, string newPrice)
+        public int DiscountCounter(double oldPrice, double newPrice)
         {
-            var oldPriceIsNull = string.IsNullOrEmpty(oldPrice);
-            var newPriceIsNull = string.IsNullOrEmpty(newPrice);
-            if (!oldPriceIsNull && !newPriceIsNull)
-            {
-                double _old = double.Parse(oldPrice);
-                double _new = double.Parse(newPrice);
-                double result;
+            double _old = oldPrice;
+            double _new = newPrice;
+            double result;
                 if (_new <= _old) result = (_new / _old) * 100;
                 else result = (_old / _new) * 100;
-                return (-(Convert.ToInt32(100-result))).ToString();
-            }
-            return "0";
+                return -(Convert.ToInt32(100-result));
         }
     }
 }
--- DataBase/ProductDB.cs
+++ DataBase/ProductDB.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Product = DiscountAggregator.AbstractTypes.Product;
 using IProductDBSetting = DiscountAggregator.DataBase.DataBaseApi.IProductDBSetting;
 using DiscountAggregator.AbstractTypes;
+using MongoDB.Bson;
 
 namespace DiscountAggregator.DataBase
 {
